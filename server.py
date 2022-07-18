ba6b916bcbd08d42fe49647a7b0f90142a93d181
Mario <mariobattlefieldm@gmail.com>
2022-07-18 12:58:48 +0000

    Initial files

---
+++ server.py
@@ -0,0 +1,15 @@
+import socket
+
+def main():
+    SERVER = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM
+        )
+    
+    
+
+
+    return
+
+if __name__ == "__main__":
+    main()
